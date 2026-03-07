f97aa23d1eee32747cb20dac7c3f02e7a1d569e4
Matthew Love <matthew.love@colorado.edu>
2026-03-06 17:19:14 -0800

    logo reformatting

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -88,7 +88,7 @@ html_theme_options = {
 # html_logo = "_static/logo.png"
 html_title = "Fetchez Documentation"
 # #html_logo = "_static/fetchez_logo_micro.svg"
-html_logo = "_static/cudem_logo.svg"
+html_logo = "_static/continuous_dems_logo.svg"
 
 html_theme_options = {
     "logo": {
