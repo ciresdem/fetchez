2f70f300f709c4f49255e33f108f76a985e38f76
Matthew Love <matthew.love@colorado.edu>
2026-04-24 14:14:47 -0700

    revert recipe logging

--- src/fetchez/recipe.py
+++ src/fetchez/recipe.py
@@ -26,28 +26,18 @@ logger = logging.getLogger(__name__)
 
 # This is duplicated in fetchez.cli
 # We should move this to utils
-def setup_logging(quiet=False, verbose=False):
-    if quiet:
-        log_level = logging.WARNING
-    elif verbose:
-        log_level = logging.DEBUG
-    else:
-        log_level = logging.INFO
+def setup_logging(verbose=False):
+    log_level = logging.INFO if verbose else logging.WARNING
 
-    logger = logging.getLogger("fetchez")
+    logger = logging.getLogger()
     logger.setLevel(log_level)
-
-    logger.propagate = False
 
     if logger.hasHandlers():
         logger.handlers.clear()
 
     handler = TqdmLoggingHandler()
-
-    # formatter = logging.Formatter("[ %(levelname)s ] %(name)s: %(message)s")
     formatter = logging.Formatter("[ %(levelname)s ] %(module)s: %(message)s")
     handler.setFormatter(formatter)
-
     logger.addHandler(handler)
 
 
@@ -81,7 +71,7 @@ class Recipe:
         self.config = config
         self.base_dir = base_dir or os.getcwd()
         self.name = self.config.get("project", {}).get("name", "Unnamed_Recipe")
-        setup_logging(False, False)
+        setup_logging(True)
 
     @classmethod
     def from_file(cls, config_source):
