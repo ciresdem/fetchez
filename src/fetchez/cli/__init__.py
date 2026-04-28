b51509b076ab3623d129b93eb8690ee3594dabe7
Matthew Love <matthew.love@colorado.edu>
2026-04-27 17:17:43 -0700

    cli_opts from _cli

--- src/fetchez/cli/__init__.py
+++ src/fetchez/cli/__init__.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 """
-fetchez.click
+fetchez.cli
 ~~~~~~~~~~~
 
 The main command-line interface for the Fetchez framework.
@@ -128,55 +128,3 @@ cli.add_command(bundles_group, name="bundles")
 
 if __name__ == "__main__":
     cli()
-# #!/usr/bin/env python
-# # -*- coding: utf-8 -*-
-
-# """
-# fetchez.click
-# ~~~~~~~~~~~
-# The main command-line interface for the Fetchez framework.
-# """
-
-# import click
-# import logging
-
-# from fetchez.utils import _cli_logo, TqdmLoggingHandler
-# from fetchez import __version__
-
-# from .run import run_group
-# from .pipeline import pipeline_group
-
-# # from .discover import hooks_group, modules_group
-# from .modules import modules_group
-# from .hooks import hooks_group
-# from .schemas import schemas_group
-
-# from .recipes import recipes_group
-# from .presets import presets_group
-# from .bundles import bundles_group
-
-
-# @click.group(
-#     help=f"\b{_cli_logo('fetchez', 'Fetch geospatial data with ease.', __version__)}",
-# )
-# @click.version_option(package_name="fetchez")
-# def cli():
-#     """Fetchez CLI."""
-
-#     setup_logging()
-
-
-# cli.add_command(run_group, name="run")
-# cli.add_command(pipeline_group, name="pipeline")
-
-# cli.add_command(modules_group, name="modules")
-# cli.add_command(hooks_group, name="hooks")
-# cli.add_command(schemas_group, name="schemas")
-
-# cli.add_command(recipes_group, name="recipes")
-# cli.add_command(presets_group, name="presets")
-# cli.add_command(bundles_group, name="bundles")
-
-
-# if __name__ == "__main__":
-#     cli()
