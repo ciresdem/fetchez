db72a8c4be9de1a56472482df356afe296721cf2
Matthew Love <matthew.love@colorado.edu>
2026-05-22 11:38:50 -0700

    commentary and stream-init

--- src/fetchez/core.py
+++ src/fetchez/core.py
@@ -925,7 +925,10 @@ def run_fetchez(modules: List["FetchModule"], threads: int = 3, global_hooks=Non
                             item.get("stream") is not None
                             for _, item in current_entries
                         )
-                        if not has_stream:
+                        # Make sure a custom stream-init isn't set by the entry
+                        has_stream_init = "stream-init" in [item.name for item in active_stream_hooks]
+
+                        if not has_stream and not has_stream_init:
                             try:
                                 from fetchez.registry import HookRegistry
 
