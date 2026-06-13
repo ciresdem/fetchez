e07ba94f47b4f5fe37be4de935553bdf7e9745f2
Matthew Love <matthew.love@colorado.edu>
2026-06-12 22:43:50 -0700

    ruff format

--- src/fetchez/hooks/set_datatype.py
+++ src/fetchez/hooks/set_datatype.py
@@ -39,7 +39,9 @@ class SetDatatype(FetchHook):
     meta_category = "metadata"
     meta_aliases = ["set_datatype"]
 
-    def __init__(self, data_type=None, default=None, rules=None, match_key=None, **kwargs):
+    def __init__(
+        self, data_type=None, default=None, rules=None, match_key=None, **kwargs
+    ):
         super().__init__(**kwargs)
         self.default = default if default is not None else data_type
         self.match_key = match_key
